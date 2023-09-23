1089d80c5250c05bc9c9dcfbe25f33e0abd70868
AdilDevStuff <94475453+AdilDevStuff@users.noreply.github.com>
2023-09-23 21:57:47 +0500

    Coin Follow Player When In Range

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -4,14 +4,17 @@ extends Area3D
 
 # You can change these values from inspector!
 @export_category("Properties")
+@export var follow_speed := 6
 @export var amplitude := 0.2
 @export var frequency := 4
 
 var time_passed = 0
-
+var is_in_range = false
 
 # Vector
 var initial_position := Vector3.ZERO
+
+@onready var player := get_tree().get_first_node_in_group("Player")
 
 # ---------- FUNCTIONS ---------- #
 
@@ -22,12 +25,18 @@ func _process(delta):
 	coin_hover(delta) # Call the coin_hover function
 	rotate_y(deg_to_rad(3))
 	
+	if is_in_range:
+		follow_player(delta)
+	
 # Coin Hover Animation
 func coin_hover(delta):
 	time_passed += delta
 	
 	var new_y = initial_position.y + amplitude * sin(frequency * time_passed)
 	position.y = new_y
+
+func follow_player(delta):
+	position += global_position.direction_to(player.global_position) * follow_speed * delta
 
 # ---------- SIGNALS ---------- #
 
@@ -37,3 +46,9 @@ func _on_body_entered(body):
 		GameManager.add_score()
 		AudioManager.coin_sfx.play()
 		queue_free()
+
+func _on_range_body_entered(body):
+	if body.is_in_group("Player"):
+		is_in_range = true
+	else:
+		is_in_range = false
