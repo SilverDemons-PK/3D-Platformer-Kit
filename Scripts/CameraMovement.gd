601ed64b9decd479d785d592ff7d02b772495189
Rayyan Aziz <itsRayan06@gmail.com>
2023-09-23 21:14:43 +0500

    Changed camera Y Axis from -90 to -60

--- Scripts/CameraMovement.gd
+++ Scripts/CameraMovement.gd
@@ -25,7 +25,7 @@ func _ready():
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotation_degrees.x -= event.relative.y * mouse_sensitivity
-		rotation_degrees.x = clamp(rotation_degrees.x, -90, -0)
+		rotation_degrees.x = clamp(rotation_degrees.x, -60, -0)
 		
 		rotation_degrees.y -= event.relative.x * mouse_sensitivity
 		rotation_degrees.y = wrapf(rotation_degrees.y, 0, 360)
